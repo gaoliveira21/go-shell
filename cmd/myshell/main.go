a062ba60d758502d8d495ca5d3b134214ce92cc4
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2024-06-09 13:05:50 -0300

    move builtins to a separate file

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,65 +7,10 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strconv"
 	"strings"
 )
 
 var builtIns map[string]func(args []string)
-
-func exit(args []string) {
-	code := 0
-	if len(args) > 0 {
-		c, err := strconv.Atoi(args[0])
-		if err == nil {
-			code = c
-		}
-	}
-
-	os.Exit(code)
-}
-
-func echo(args []string) {
-	fmt.Println(strings.Join(args, " "))
-}
-
-func typeBuiltIn(args []string) {
-	cmd := args[0]
-
-	for k := range builtIns {
-		if k == cmd {
-			fmt.Printf("%s is a shell builtin\n", cmd)
-			return
-		}
-	}
-
-	pathEnv := os.Getenv("PATH")
-	paths := strings.Split(pathEnv, ":")
-
-	for _, path := range paths {
-		entries, err := os.ReadDir(path)
-		if err != nil {
-			continue
-		}
-
-		for _, e := range entries {
-			if !e.IsDir() && e.Name() == cmd {
-				fmt.Printf("%s is %s\n", cmd, fmt.Sprintf("%s/%s", path, cmd))
-				return
-			}
-		}
-	}
-
-	fmt.Printf("%s: not found\n", cmd)
-}
-
-func cd(args []string) {
-	path := strings.Join(args, "")
-
-	if err := os.Chdir(path); err != nil {
-		fmt.Printf("cd: %s: No such file or directory\n", path)
-	}
-}
 
 func readLine() string {
 	reader := bufio.NewReader(os.Stdin)
